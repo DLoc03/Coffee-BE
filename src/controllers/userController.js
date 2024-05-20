84693ce94343e9ede2150e1638f69d37fa3d1f17
DLoc03 <locn9730@gmail.com>
2024-05-21 04:02:05 +0700

    Initial commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,71 @@
+import db from "../models/index";
+import CRUDService from "../services/CRUD-Service";
+import userService from "../services/user-Service";
+
+let handleLogin = async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(500).json({
+      errorCode: 1,
+      message: "Email hoặc mật khẩu trống!",
+    });
+  }
+
+  let userData = await userService.handleUserLogin(email, password);
+  console.log(userData);
+
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {},
+  });
+};
+
+let getAllUser = async (req, res) => {
+  let id = req.query.id;
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameter",
+      users: [],
+    });
+  }
+  let users = await userService.getAllUser(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "Done",
+    users,
+  });
+};
+
+let handleCreateNewUser = async (req, res) => {
+  let message = await userService.createNewUser(req.body);
+  return res.status(200).json(message);
+};
+
+let handleEditUser = async (req, res) => {
+  let data = req.body;
+  let message = await userService.updateUser(data);
+};
+
+let handleDeleteUser = async (req, res) => {
+  if (!req.body.id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameters",
+    });
+  }
+
+  let message = await userService.deleteUser(req.body.id);
+  return res.status(200).json(message);
+};
+
+module.exports = {
+  handleLogin: handleLogin,
+  getAllUser: getAllUser,
+  handleCreateNewUser: handleCreateNewUser,
+  handleEditUser: handleEditUser,
+  handleDeleteUser: handleDeleteUser,
+};
---
+++ src/route/web.js
@@ -0,0 +1,34 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
+import storeController from "../controllers/storeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomePage);
+  router.get("/about", homeController.getAboutPage);
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+  router.get("/get-crud", homeController.displayCRUD);
+  router.get("/edit-crud", homeController.editCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+  router.get("/del-crud", homeController.delCRUD);
+
+  //Users
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-user", userController.getAllUser);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
+
+  //Stores
+  router.get("/api/get-all-store", storeController.handleGetAllStore);
+  router.post("/api/create-new-store", storeController.handleCreateNewStore);
+  router.put("/api/edit-store", storeController.handleEditStore);
+  router.delete("/api/delete-store", storeController.handleDeleteStore);
+
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
